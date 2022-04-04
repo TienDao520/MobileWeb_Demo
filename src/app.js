0f079038c5ec71a7882b3180a6b9b5d7c4295a47
Tien Dao <tiendao5240@gmail.com>
2022-04-04 15:55:22 -0400

    Added elements and querySelector

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
-const setupPage = () => {}
+const elements = {
+    textInpt: null,
+    rangeInpt: null,
+    switchInpt: null,
+    radio1Inpt: null,
+    radio2Inpt: null,
+    saveBtn: null,
+    loadBtn: null,
+
+}
+
+let state = {
+    text: null,
+    range: null,
+    switch: null,
+    radio: null,
+}
+
+const setupPage = () => {
+    elements.textInpt = document.querySelector('#text');
+    elements.rangeInpt = document.querySelector('#range');
+    elements.switchInpt = document.querySelector('#switch');
+    elements.radio1Inpt = document.querySelector('#radio-1');
+    elements.radio2Inpt = document.querySelector('#radio-2');
+    elements.saveBtn = document.querySelector('#save');
+    elements.loadBtn = document.querySelector('#load');
+}
 
 document.addEventListener('init', setupPage);
+
+
+
+
