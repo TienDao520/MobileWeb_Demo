023ba6be9ea76c1b3ec6075f8a005da0dbe3c2a2
Tien Dao <tiendao5240@gmail.com>
2022-04-13 10:54:05 -0400

    Added elements and query selector

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
-const setupPage = () => {}
+let elements = {
+    networkType: null,
+    networkOnline: null,
+    networkDownlink: null,
+
+    batteryLevel: null,
+    batteryCharging: null,
+    batteryTime: null,
+}
+
+const setupPage = () => {
+    elements = {
+        networkType: document.querySelector('#networkType'),
+        networkOnline: document.querySelector('#networkOnline'),
+        networkDownlink: document.querySelector('#networkDownlink'),
+
+        batteryLevel: document.querySelector('#batteryLevel'),
+        batteryCharging: document.querySelector('#batteryCharging'),
+        batteryTime: document.querySelector('#batteryTime'),
+
+    }
+}
 
 document.addEventListener('init', setupPage);
